bdf6794c9ace2e6ad57a9ca8d270b7c0ada9a451
SkyVoidOfStars <whosmadureira@gmail.com>
2025-05-04 09:36:33 -0300

    Comandos retrabalhados

--- commands/nome.py
+++ commands/nome.py
@@ -1,19 +1,19 @@
 import discord
 from discord.ext import commands
 from db import NewSession, User
+from config import *
 
 def setup_commands(bot:commands.Bot):
-    @bot.command(name='nome', description='Altera o nome do personagem')
-    async def nome(ctx:discord.ext.commands.Context, *, name:str):
+    @bot.tree.command(name='nome', description='Altera o nome de um personagem')
+    @discord.app_commands.checks.has_any_role(*AllowedRoles)
+    async def nome(interaction:discord.Interaction, usuário:discord.User, nome:str):
         session = NewSession()
-        user = session.query(User).filter_by(user_id=ctx.author.id).first()
+        user = session.query(User).filter_by(user_id=usuário.id).first()
         if not user:
             session.close()
-            await ctx.message.delete()
-            await ctx.send(f'{ctx.author.mention} Você não está cadastrado no sistema!')
+            await interaction.response.send_message(f'{usuário.mention} não está cadastrado no banco de dados!', ephemeral=True, delete_after=5)
             return
-        user.user_character_name = name
+        user.user_character_name = nome
         session.commit()
         session.close()
-        await ctx.message.delete()
-        await ctx.send(f'{ctx.author.mention} Nome alterado para `{name}` com sucesso!')
+        await interaction.response.send_message(f'Nome de {usuário.mention} alterado para `{nome}` com sucesso!', ephemeral=True, delete_after=5)
