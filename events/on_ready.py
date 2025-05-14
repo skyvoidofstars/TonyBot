21ecfdcc715cfa8adeb761622e0d69d3d546ea30
SkyVoidOfStars <whosmadureira@gmail.com>
2025-05-13 22:06:18 -0300

    Mensagem de inicialização modificada

--- events/on_ready.py
+++ events/on_ready.py
@@ -6,13 +6,27 @@ from git import Repo
 def get_latest_commit_info(repo_path="."):
     repo = Repo(repo_path)
     commit = repo.head.commit
-    return commit.hexsha[:7], commit.message.strip()
+    
+    diff = commit.diff(None, create_patch=False)
+
+    lines = []
+
+    for item in diff.iter_change_type('A'):
+        lines.append(f"\u001b[2;32m\u001b[1;32m+ {item.b_path}\u001b[0m")
+    for item in diff.iter_change_type('M'):
+        lines.append(f"\u001b[2;33m\u001b[1;33m± {item.b_path}\u001b[0m")
+    for item in diff.iter_change_type('D'):
+        lines.append(f"\u001b[2;31m\u001b[1;31m- {item.a_path}\u001b[0m")
+
+    commit_summary = "```ansi\n" + "\n".join(lines) + "\n```"
+    
+    return commit.hexsha[:10], commit.message.strip(), commit.author.name, commit_summary
 
 def setup_events(bot:discord.ext.commands.Bot):
     @bot.event
     async def on_ready():
         sincs = await bot.tree.sync()
-        commit_hash, commit_msg = get_latest_commit_info()
+        commit_hash, commit_msg, commit_author, commit_summary = get_latest_commit_info()
         print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {len(sincs)} comandos sincronizados')
         print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} Bot conectado como {bot.user}')
-        await bot.get_guild(LogGuild).get_channel(LogChannel).send(f'<@129620949090697216>\nBot (re)inicializado às {datetime.now().strftime("%H:%M:%S")}\n{len(sincs)} comandos sincronizados\n\nÚltimo commit: `{commit_hash}`: {commit_msg}')
+        await bot.get_guild(LogGuild).get_channel(LogChannel).send(f'Bot (re)inicializado às {datetime.now().strftime("%H:%M:%S")}\n{len(sincs)} comandos sincronizados\n\nÚltimo commit: `{commit_hash}`por {commit_author}.\n\n{commit_msg}\n{commit_summary}\n||<@129620949090697216>||')
