6a1dce49fb0957fb4b4817257f97ae6c68a4eb71
SkyVoidOfStars <whosmadureira@gmail.com>
2025-05-27 12:44:02 -0300

    Correção de código

--- events/on_ready.py
+++ events/on_ready.py
@@ -11,11 +11,11 @@ def get_latest_commit_info(repo_path="."):
 
     lines = []
 
-    for item in diff.iter_change_type('A'):
+    for item in diff.iter_change_type('D'):
         lines.append(f"\u001b[2;32m\u001b[1;32m+ {item.b_path}\u001b[0m")
     for item in diff.iter_change_type('M'):
         lines.append(f"\u001b[2;33m\u001b[1;33m± {item.b_path}\u001b[0m")
-    for item in diff.iter_change_type('D'):
+    for item in diff.iter_change_type('A'):
         lines.append(f"\u001b[2;31m\u001b[1;31m- {item.a_path}\u001b[0m")
 
     commit_summary = "```ansi\n" + "\n".join(lines) + "\n```"
