80e96849e66d6afe20d3ea98b3cf28002931c07b
SkyVoidOfStars <whosmadureira@gmail.com>
2025-08-06 18:58:44 -0300

    Corrreção na observabilidade Slash commands adionado na observabilidade de discord.MissingAnyRoles

--- events/on_command_error.py
+++ events/on_command_error.py
@@ -8,30 +8,20 @@ def setup_events(bot: commands.Bot):
     async def on_app_command_error(interaction: discord.Interaction, error):
         actual_error = getattr(error, 'original', error)
 
-        if isinstance(actual_error, commands.MissingAnyRole):
-            await (
-                bot.get_guild(interaction.guild_id)
-                .get_channel(interaction.channel_id)
-                .send(
-                    f'{interaction.user.mention} Você não tem permissão para usar o comando {interaction.command.name}!'
+        if isinstance(actual_error, commands.MissingAnyRole) or isinstance(actual_error, discord.app_commands.MissingAnyRole):
+            await interaction.response.send_message(
+                    content=f'{interaction.user.mention} Você não tem permissão para usar o comando {interaction.command.name}!'
                 )
-            )
         elif isinstance(actual_error, discord.NotFound):
             await (
-                bot.get_guild(interaction.guild_id)
-                .get_channel(interaction.channel_id)
-                .send(
+                interaction.response.send_message(
                     f'{interaction.user.mention} A interação do comando {interaction.command.name} falhou por tempo esgotado! Experimente tentar novamente.'
                 )
             )
         else:
-            await (
-                bot.get_guild(interaction.guild_id)
-                .get_channel(interaction.channel_id)
-                .send(
-                    f'{interaction.user.mention} Erro ao executar o comando {interaction.command.name}!'
+            await interaction.response.send_message(
+                    content=f'{interaction.user.mention} Erro ao executar o comando {interaction.command.name}!'
                 )
-            )
             log_error = actual_error if actual_error is not error else error
             await (
                 bot.get_guild(interaction.guild.id)
